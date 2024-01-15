2593b18898120df8888a825cdf25f311f6be8203
Kauê de Magalhães Brandão <kauedemagalhaes27@gmail.com>
2024-01-15 11:08:05 -0300

    Refactor ShortUrlController and add ShortUrlFactory and CreateTest

--- tests/Feature/ShortUrl/CreateTest.php
+++ tests/Feature/ShortUrl/CreateTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature\ShortUrl;
 
+use App\Models\ShortUrl;
 use Facades\App\Actions\CodeGenerator;
 use Symfony\Component\HttpFoundation\Response;
 use Tests\TestCase;
@@ -40,5 +41,22 @@ class CreateTest extends TestCase
       ->assertJsonValidationErrors([
         'url' => __('validation.url', ['attribute' => 'url']),
       ]);
+  }
+
+  /** @test */
+  public function it_should_return_the_existed_code_if_the_url_is_the_same()
+  {
+    ShortUrl::factory()->create([
+      'url' => 'https://www.google.com',
+      'short_url' => config('app.url') . '/123456',
+      'code' => '123456',
+    ]);
+
+    $this->postJson(route('api.short-url.store'), ['url' => 'https://www.google.com'])
+      ->assertJson([
+        'shortUrl' => config('app.url') . '/123456',
+      ]);
+
+    $this->assertDatabaseCount('short_urls', 1);
   }
 }
